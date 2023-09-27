39fa804496cf72879f0a0df0defe6ac50b5fa675
sandesh034 <dhitalsandesh1@gmail.com>
2023-09-27 13:25:39 +0545

    My first commit

---
+++ modal/script.js
@@ -0,0 +1,8 @@
+
+const openModal=()=>{
+    document.querySelector('.modals').classList.add('show-modal')
+}
+
+const closeModal = () => {
+    document.querySelector('.modals').classList.remove('show-modal');
+}
