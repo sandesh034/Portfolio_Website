50becb2c6c4858a54e6279bb59290e6933dc0231
sandesh034 <dhitalsandesh1@gmail.com>
2023-10-05 11:49:19 +0545

    ui improved

--- modal/script.js
+++
@@ -1,8 +0,0 @@
-
-const openModal=()=>{
-    document.querySelector('.modals').classList.add('show-modal')
-}
-
-const closeModal = () => {
-    document.querySelector('.modals').classList.remove('show-modal');
-}
